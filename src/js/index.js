2d11cf2dbcf1d279e3ce5fd502d640c8aafd74de
misumi <misumi@logical-studio.com>
2021-10-21 21:09:46 +0900

    light

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,15 @@
 import {
   WebGLRenderer,
-  PerspectiveCamera,
   Scene,
-  PointLight,
-  BoxGeometry,
-  MeshLambertMaterial,
-  Mesh
+  Mesh,
+  OrthographicCamera,
+  PlaneGeometry,
+  ShaderMaterial,
+  Vector2
 } from 'three'
+
+import vertexSource from './shaders/shader.vert'
+import fragmentSource from './shaders/shader.frag'
 
 const width = window.innerWidth
 const height = window.innerHeight
@@ -16,46 +19,86 @@ const renderer = new WebGLRenderer()
 renderer.setSize(width, height)
 renderer.setPixelRatio(window.devicePixelRatio)
 
-// canvasにレンダラー追加
 const container = document.getElementById('canvas-container')
 container.appendChild(renderer.domElement)
 
 // カメラ作成
-const camera = new PerspectiveCamera(60, width / height, 1, 10)
-camera.position.z = 3
+const camera = new OrthographicCamera(-1, 1, 1, -1, 0, -1)
 
 // シーンを作成
 const scene = new Scene()
 
-// ライトを作成
-const light = new PointLight(0x00ffff)
-light.position.set(2, 2, 2)
+// 平面をつくる（幅, 高さ, 横分割数, 縦分割数）
+const geo = new PlaneGeometry(2, 2, 1, 1)
 
-// ライトをシーンに追加
-scene.add(light)
+const mouse = new Vector2(0.5, 0.5)
+let targetRadius = 0.005
 
-// 立方体のジオメトリを作成
-const geo = new BoxGeometry(1, 1, 1)
+const uniforms = {
+  uAspect: {
+    value: width / height
+  },
+  uTime: {
+    value: 0.0
+  },
+  uMouse: {
+    value: new Vector2(0.5, 0.5)
+  },
+  uRadius: {
+    value: targetRadius
+  }
+}
 
-// マテリアルを作成
-const mat = new MeshLambertMaterial({ color: 0xffffff })
+// シェーダーソースを渡してマテリアルを作成
+const mat = new ShaderMaterial({
+  uniforms: uniforms,
+  vertexShader: vertexSource,
+  fragmentShader: fragmentSource,
+  wireframe: false
+})
 
-// ジオメトリとマテリアルからメッシュを作成
 const mesh = new Mesh(geo, mat)
 
 // メッシュをシーンに追加
 scene.add(mesh)
 
-const loop = () => {
-  requestAnimationFrame(loop)
+const mouseMoved = (x, y) => {
+  // 左上原点から左下原点に変換
+  mouse.x = x / width
+  mouse.y = 1.0 - y / height
+}
+
+const mousePressed = (x, y) => {
+  mouseMoved(x, y)
+  targetRadius = 0.25 // マウスを押したら半径の目標値を大きく
+}
+
+const mouseReleased = (x, y) => {
+  mouseMoved(x, y)
+  targetRadius = 0.005 // マウスを押したら半径の目標値をデフォルト値に
+}
+
+const render = () => {
+  requestAnimationFrame(render)
 
   const sec = performance.now() / 1000
 
-  mesh.rotation.x = sec * (Math.PI / 4)
-  mesh.rotation.y = sec * (Math.PI / 4)
+  uniforms.uTime.value = sec
+  uniforms.uMouse.value.lerp(mouse, 0.2)
+  uniforms.uRadius.value += (targetRadius - uniforms.uRadius.value) * 0.2
 
   // 描画ループ
   renderer.render(scene, camera)
 }
 
-loop()
+render()
+
+window.addEventListener('mousemove', (e) => {
+  mouseMoved(e.clientX, e.clientY)
+})
+window.addEventListener('mousedown', (e) => {
+  mousePressed(e.clientX, e.clientY)
+})
+window.addEventListener('mouseup', (e) => {
+  mouseReleased(e.clientX, e.clientY)
+})
